46f5e6c570f30a54a9d0d04eeb1abae28ffaed81
Pramod Gobburi <pramod.gobburi@gmail.com>
2019-02-01 16:24:25 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'about#index', as: 'home'
+  get 'portfolio', to: 'posts#index', as: 'portfolio'
+end
