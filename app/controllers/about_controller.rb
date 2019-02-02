7d3c9c0a4f1d31c46af78ed556af40c2ad9128bb
Pramod Gobburi <pramod.gobburi@gmail.com>
2019-02-01 21:21:04 -0600

    Update 2

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -6,5 +6,6 @@ class AboutController < ApplicationController
 
     def download_resume
         send_file "#{Rails.root}/app/assets/docs/PramodGobburi_Resume.pdf", type: "application/pdf", x_sendfile: true
+        render
     end
 end
