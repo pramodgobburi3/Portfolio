97f3715ea0b903219a005f35f23ab78b4c10a030
Pramod Gobburi <pramod.gobburi@gmail.com>
2019-02-01 18:23:55 -0600

    Added footer

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,4 +3,8 @@ class AboutController < ApplicationController
     def index
 
     end
+
+    def download_resume
+        send_file "#{Rails.root}/app/assets/docs/PramodGobburi_Resume.pdf", type: "application/pdf", x_sendfile: true
+    end
 end
